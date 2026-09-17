78e255cea7d6a005fb3d7427957f647490ff0e29
justocker <julianstockerq@gmail.com>
2026-09-17 21:07:29 +0200

    added fps

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt: float = 0.0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -13,7 +16,10 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
 
+    
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
