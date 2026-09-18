ad9e9eb925073b69e27a7b6dbcc96f271b759b49
justocker <julianstockerq@gmail.com>
2026-09-18 17:59:20 +0200

    added containers/groups

--- main.py
+++ main.py
@@ -5,12 +5,20 @@ from player import Player
 
 def main():
     pygame.init()
+
+    updatable = pygame.sprite.Group() # this will hold all the objects that can be updated
+    drawable = pygame.sprite.Group() # this will hold all the objects that can be drawn
+    Player.containers = (updatable, drawable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     clock = pygame.time.Clock()
     dt: float = 0.0
 
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
+
+    
 
     while True:
         log_state()
@@ -18,19 +26,17 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+
+        for draws in drawable:
+            draws.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
 
         
         
-
-    
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
